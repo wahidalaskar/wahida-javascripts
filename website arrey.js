a5b4932d064e0bc90e907d9c0f9114f5e1b8a8cd
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-14 14:25:31 +0100

    first commit

---
+++ website arrey.js
@@ -0,0 +1,20 @@
+let favWebSites = ["facebook",
+"twitter",
+"microsoft"];
+console.log(favWebSites)
+favWebSites.push("instagram", "java")
+console.log(favWebSites);
+favWebSites.pop();
+
+console.log(favWebSites)
+
+favWebsites.push( "instagram", "java")
+
+consoles.log(favWebsites); 
+favWebsites.pop();
+
+console.log(favWebsites)
+
+favWebSites;unshift("yahoo") ;
+
+console.log(favWebsites) 
